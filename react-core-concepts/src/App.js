489133d8ccd06b973d7a30ca16fc91bc39884a72
Nazmul Hoque <coder.nazmul@gmail.com>
2020-03-12 14:55:39 +0600

    entry point is customized

--- react-core-concepts/src/App.js
+++ react-core-concepts/src/App.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './Main.css';
 
 function App() {
+  var clickBtn = {
+    textDecoration: 'none',
+    fontSize: '18px',
+    padding: '10px 30px',
+    borderRadius: '10px',
+    backgroundColor: 'salmon',
+    color: 'white'
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2><span className="highliter">Hello</span> <span className="test">world</span>, I am here!</h2>
+        <p>I am <span style={{ color: 'cyan', fontWeight: 'bold' }}>Nazmul Hoque</span></p>
+        <p>If you interest to know about me, please</p><a style ={clickBtn} href="#">Click Here</a>
       </header>
     </div>
   );
